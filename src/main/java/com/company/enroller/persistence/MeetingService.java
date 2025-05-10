5e9c2276303747653f43f901810209f441e188ff
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-10 08:46:13 +0200

    Update MeetingService.java

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +22,37 @@ public class MeetingService {
 		Query query = this.session.createQuery(hql);
 		return query.list();
 	}
+
+	public Meeting getMeetingById(long id) {
+		String hql = "FROM Meeting WHERE id = :id";
+		Query query = this.session.createQuery(hql);
+		query.setParameter("id", id);
+		return (Meeting) query.uniqueResult();
+	}
+
+	public Meeting createMeeting(Meeting meeting) {
+		Transaction tx = session.beginTransaction();
+		session.save(meeting);
+		tx.commit();
+		return meeting;
+	}
+	public Meeting removeMeetingById(long id) {
+		Transaction tx = session.beginTransaction();
+		session.delete(getMeetingById(id));
+		tx.commit();
+		return null;
+	}
+
+	public Meeting updateMeetingById(Meeting meeting, long id) {
+		Transaction tx = session.beginTransaction();
+		Meeting updatedMeeting = getMeetingById(id);
+		updatedMeeting.setTitle(meeting.getTitle());
+		updatedMeeting.setDescription(meeting.getDescription());
+		updatedMeeting.setDate(meeting.getDate());
+		session.update(updatedMeeting);
+		tx.commit();
+		return updatedMeeting;
+	}
+
 
 }
