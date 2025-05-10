664e462024b33258e424208010338ccb59d4a529
Bartek5765 <bprzeczek@student.agh.edu.pl>
2025-05-10 08:14:47 +0200

    Add files via upload

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,30 +1,63 @@
 package com.company.enroller.controllers;
 
-import com.company.enroller.model.Participant;
+import com.company.enroller.model.Meeting;
 import com.company.enroller.persistence.MeetingService;
-import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/meetings")
 public class MeetingRestController {
+    @Autowired
+    MeetingService meetingService;
 
-@Autowired
-MeetingService meetingService;
+    @RequestMapping(value="", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetings() {
+        Collection<Meeting> meetings = meetingService.getAll();
+        return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+    }
 
-@RequestMapping(value = "", method = RequestMethod.GET)
-public ResponseEntity<?> getAllMeetings(
-        @RequestParam(value = "sortBy", required = false) String sortBy,
-        @RequestParam(value = "sortOrder", required = false, defaultValue = "") String sortOrder,
-        @RequestParam(value = "key", required = false, defaultValue = "") String key) {
-    Collection<Participant> participants = participantService.getAll(sortBy, sortOrder, key);
-    return new ResponseEntity<>(participants, HttpStatus.OK);
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetingById(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.getMeetingById(id);
+        if (meeting == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="", method = RequestMethod.POST)
+    public ResponseEntity<?> createMeeting(@RequestBody Meeting meeting) {
+        Meeting foundMeeting = meetingService.getMeetingById(meeting.getId());
+        if (foundMeeting != null) {
+            return new ResponseEntity("Unable to add meeting", HttpStatus.CONFLICT);
+        }
+        Meeting meeting1 = meetingService.createMeeting(meeting);
+        return new ResponseEntity<Meeting>(meeting1, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> removeMeetingById(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.getMeetingById(id);
+        if(meeting == null) {
+            return new ResponseEntity<>("unable to remove meeting", HttpStatus.NOT_FOUND);
+        }
+        Meeting meeting1 = meetingService.removeMeetingById(id);
+        return new ResponseEntity<Meeting>(meeting1, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateMeetingById(@PathVariable("id") long id, @RequestBody Meeting meeting) {
+        Meeting foundMeeting = meetingService.getMeetingById(id);
+        if (foundMeeting != null) {
+            return new ResponseEntity<>("Unable to update meeting", HttpStatus.CONFLICT);
+        }
+        Meeting meeting1 = meetingService.updateMeetingById(meeting,id);
+        return new ResponseEntity<>(meeting1, HttpStatus.OK);
+    }
 }
+
